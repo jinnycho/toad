a5dc88873b43f8bcff8cac4b85f8ab783738a1da
Charles Chamberlain <charles@nrwhl.xyz>
2023-04-06 17:11:51 -0400

    Abstract svg element creation into new function and add some styles

--- draw/drawing-00.js
+++ draw/drawing-00.js
@@ -3,7 +3,9 @@ import * as toad from "../lib/toad.js";
 // Starting out by drawing a circle
 
 function main() {
-  toad.drawCircle({ x: 100, y: 100, r: 40 })
+  toad.drawCircle({ x: 100, y: 100, r: 40 })._transform('rotate(-10 50 100)');
+  toad.drawCircle({ x: 100, y: 100, r: 40 })._transform('rotate(-10 300 300)')
+  toad.drawCircle({ x: 100, y: 100, r: 40 })._transform('translate(36 34)')
 }
 
 window.main = main
