dd0b11c83ea26769cc43816592f91857d2ba79b2
Charles Chamberlain <charles@nrwhl.xyz>
2023-04-06 22:09:55 -0400

    Address comments, add small text primitive, and change transform API slightly

--- draw/test-00.js
+++ draw/test-00.js
@@ -1,12 +1,12 @@
 import * as toad from "../lib/toad.js";
 
-function test() {
+function main() {
   toad.strokeWidth(2, () => {
 
     toad.drawLine({ from: { x: 300, y: 100 }, to: { x: 300, y: 300 } })
 
     toad.strokeWidth(40, () => {
-      toad.drawLine({ from: { x: 400, y: 100 }, to: { x: 400, y: 300 } }).scale(0.4).scaleXY({ x: 4, y: 1 }).translate({ x: -300, y: 200 })
+      toad.drawLine({ from: { x: 400, y: 100 }, to: { x: 400, y: 300 } })._transform.scale(0.4).scaleXY({ x: 4, y: 1 }).translate({ x: -300, y: 200 })
     })
 
     toad.group(() => {
@@ -14,15 +14,22 @@ function test() {
       toad.stroke('red', () => {
         toad.drawLine({ from: { x: 600, y: 100 }, to: { x: 600, y: 300 } })
       })
-    }).skewX(-20).translate({ x: 50, y: 0 })
+    })._transform.skewX(-20).translate({ x: 50, y: 0 })
 
     toad.drawCircle({ x: 100, y: 100, r: 40 })
+      ._transform
       .rotate(-70, { x: 300, y: 200 })
       .skewX(30)
       .skewY(30)
       .rotate(20, { x: 300, y: 200 })
   })
 
+  toad.fill('black', () => {
+    toad.stroke('pink', () => {
+      toad.drawText({ x: 300, y: 400 }, "hi there");
+    })
+  })
+
 }
 
-window.test = test;
+window.main = main
