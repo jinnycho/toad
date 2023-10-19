0d4368d0b86d0f68214b10a98494ad8b2054af92
Charles Chamberlain <charles@nrwhl.xyz>
2023-10-18 22:51:52 -0400

    Create art work that I like

--- drawings/drawing-03.tsx
+++ drawings/drawing-03.tsx
@@ -5,21 +5,43 @@ import { range } from "../lib/helpers.js";
 
 function drawingControls() {
   return useControls({
-    position: { x: 200, y: 200 },
-    diff: { x: 10, y: 10 },
-    n: { value: 40, min: 1, max: 300, step: 1 },
-    step: { value: 600, min: 0, max: 1800, step: 0.1 },
     delta: {
       value: { x: 3, y: 0 },
       step: 0.06,
     },
-    angle: { value: 200, min: 0, max: 600 }
   })
 }
 
 export function Drawing() {
 
-  const { n, step, delta, position, diff, angle } = drawingControls()
+  const [delta, setDelta] = React.useState({ x: 0, y: 0 });
+  // change delta according to mouse position 
+
+  React.useEffect(() => {
+    const onMouseMove = (e: MouseEvent) => {
+
+      // like this but based on the middle of the screen
+      // setDelta({
+      //   x: e.clientX / 100,
+      //   y: e.clientY / 100,
+      // });
+
+      setDelta({
+        x: (e.clientX - window.innerWidth / 2) / 100,
+        y: (e.clientY - window.innerHeight / 2) / 100,
+      });
+    };
+    window.addEventListener("mousemove", onMouseMove);
+    return () => window.removeEventListener("mousemove", onMouseMove);
+  }, []);
+
+
+  const n =  58;
+  const step = 945;
+  const angle = 494;
+  const diff = {x:28, y:28};
+  // position should be the center of the screen
+  const position = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
 
   return (<>
     <SVG>
